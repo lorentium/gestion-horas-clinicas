69712f86cd07f017873c40c7cd887e893a4d7000
notygz0 <149032450+notygz0@users.noreply.github.com>
2025-03-28 11:46:12 -0300

    Anadida la clase Clinica y CitaClinica

---
+++ Clinica.java
@@ -0,0 +1,12 @@
+public class Clinica {
+    public String nombre;
+    public String direccion;
+    public String telefono;
+
+    public Clinica(String nombre, String direccion, String telefono) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+    
+}
