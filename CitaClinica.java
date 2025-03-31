c6b474807a679e1dbb2ed549a3cea9aedfcab915
Lorenzo Lopez <l.lopez14@ufromail.cl>
2025-03-31 19:35:54 -0300

    feat: creada clase cita clinica

--- CitaClinica.java
+++ CitaClinica.java
@@ -1,2 +1,18 @@
 public class CitaClinica {
+    private Profesional profesional;
+    private Paciente paciente;
+    private Clinica clinica;
+    private String fecha;
+    private String motivo;
+
+    @Override
+    public String toString() {
+        return "CitaClinica{" +
+                "profesional=" + profesional +
+                ", paciente=" + paciente +
+                ", clinica=" + clinica +
+                ", fecha='" + fecha + '\'' +
+                ", motivo='" + motivo + '\'' +
+                '}';
+    }
 }
